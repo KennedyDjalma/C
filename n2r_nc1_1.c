ab444d4b3f73596d88ef2cda005cee1a5dd14185
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2026-07-24 10:05:03 -0300

    Operadores lógicos

---
+++ n2r_nc1_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a = -10, b =5;
+
+    if (a > 0 && b > 0) {
+        printf("Os dois numeros sao positivos. \n");
+    } else {
+        printf("Pelo menos um numero e negaivo. \n");
+    }
+}
---
+++ n2r_nc1_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+
+    if (!a) {
+        printf("A variavel A e zero. \n");
+    } else {
+        printf("A variavel e diferente de zero. \n");
+    }
+}
